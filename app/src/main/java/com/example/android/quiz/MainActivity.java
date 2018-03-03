7cc47dc3f81fce7f88efcf4f02ba1bb8eac0e081
Elena <georgiadelena@gmail.com>
2018-03-03 17:20:47 +0200

    Add else statements as specified in the project requirements and change point count

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -30,14 +30,18 @@ public class MainActivity extends AppCompatActivity {
         RadioButton miltonRadioButton = (RadioButton) findViewById(R.id.q21_radio_button);
         if (miltonRadioButton.isChecked()) score++;
 
-        //question 3 - you get 1 point when you pick all the right answers, otherwise it's a 0
+        //question 3 - you get 1 point for selecting all three right checkboxes, otherwise it's a 0
         CheckBox neverwhereBox = (CheckBox) findViewById(R.id.neverwhere);
         CheckBox graveyardBox = (CheckBox) findViewById(R.id.graveyard);
         CheckBox godsBox = (CheckBox) findViewById(R.id.gods);
         CheckBox thingsBox = (CheckBox) findViewById(R.id.things);
         CheckBox oceanBox = (CheckBox) findViewById(R.id.ocean);
-        if(neverwhereBox.isChecked() && graveyardBox.isChecked() && oceanBox.isChecked() && !godsBox.isChecked() && !thingsBox.isChecked()){
+        if(godsBox.isChecked() || thingsBox.isChecked()){
+            score += 0;
+        }else if(neverwhereBox.isChecked() && graveyardBox.isChecked() && oceanBox.isChecked()){
             score++;
+        }else{
+            score += 0;
         }
 
         //question 4
@@ -48,14 +52,18 @@ public class MainActivity extends AppCompatActivity {
         RadioButton scarletRadioButton = (RadioButton) findViewById(R.id.q53_radio_button);
         if (scarletRadioButton.isChecked()) score++;
 
-        //question 6 - you get 1 point when you pick all the right answers, otherwise it's a 0
+        //question 6 - you get 1 point for selecting all three right checkboxes, otherwise it's a 0
         CheckBox salemsBox = (CheckBox) findViewById(R.id.salems_lot);
         CheckBox omensBox = (CheckBox) findViewById(R.id.good_omens);
         CheckBox petBox = (CheckBox) findViewById(R.id.pet_sematary);
         CheckBox standBox = (CheckBox) findViewById(R.id.the_stand);
         CheckBox nightBox = (CheckBox) findViewById(R.id.night_watch);
-        if(salemsBox.isChecked() && petBox.isChecked() && standBox.isChecked() && !omensBox.isChecked() && !nightBox.isChecked()){
+        if (omensBox.isChecked() || nightBox.isChecked()){
+            score += 0;
+        }else if (salemsBox.isChecked() && petBox.isChecked() && standBox.isChecked()){
             score++;
+        }else{
+            score += 0;
         }
 
         //question 7
@@ -74,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
             EditText insertName = (EditText) findViewById(R.id.name_view);
             String name = insertName.getText().toString();
             //Show a message as a toast
-            Toast.makeText(this, "Hey " + name + "! " + getString(R.string.your_score)+ " " + score + ".", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Hey " + name + "! " + getString(R.string.your_score)+ " " + score + "/7.", Toast.LENGTH_SHORT).show();
             score = 0;
     }
 }
